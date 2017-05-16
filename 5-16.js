e4b0d8d5bed4e0524866cf588fac5f5d502cfe5d
devjanaprime <dev@primeacademy.io>
2017-05-16 10:01:37 -0500

    added readme

---
+++ 5-16.js
@@ -0,0 +1,10 @@
+console.log( 'js' );
+
+// just some variables for us to use in testing when in console
+var testy = true;
+var tester;
+
+// our deck of cards array
+var deck = [];
+
+var card = "4 of Spades";
---
+++ readme.md
@@ -0,0 +1,9 @@
+Omega JS Review 2 and Jargon
+===
+
+- data types
+- built-ins
+- strings
+- arrays
+
+cards/deck example was used in class
